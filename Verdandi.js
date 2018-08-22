8ffec871524f7008b86e5b4fbbfcb7ebf0bbfb77
Alexandre Grisey <grisey.alexandre@gmail.com>
2018-08-22 22:07:16 +0200

    Add hours/minutes/seconds support

--- Verdandi.js
+++ Verdandi.js
@@ -13,205 +13,242 @@
  * implement functions
  */
 
- /**
-  * FYI, in Norse mythology, Verdandi is one of the norns. Her name means "happening" or "present", this is why I chose this name.
-  * https: //en.wikipedia.org/wiki/verdandi
-  */
+/**
+ * FYI, in Norse mythology, Verdandi is one of the norns. Her name means "happening" or "present", this is why I chose this name.
+ * https: //en.wikipedia.org/wiki/verdandi
+ */
 export class Verdandi {
 
-  constructor(lang = "fr", options = {} ) {
-    this.DAYS = [];
-    this.MONTHS = [];
-    this.DEFAULT_DATE_SEPARATOR = options.dateSeparator != null ? options.dateSeparator : "/";
-    this.DEFAULT_HOUR_SEPARATOR = options.hourSeparator != null ? options.hourSeparator : ":";
-    this.getDaysLabelsFromLang(lang);
-    this.getMonthsLabelsFromLang(lang);
-   }
-
-  /**
-   * Return the current timestamp
-   * Easy but useful when you're not used to JS date methods
-   */
-   getCurrentTimestamp(){
-    return new Date().getTime();
-   }
-
-   /**
-    * get days labels from language
-    * To implement
-    * @param {*} lang 
-    */
-   getDaysLabelsFromLang(lang){
-    this.DAYS = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
-   }
-
-   /**
-    * get days labels from language
-    * To implement
-    * @param {*} lang 
-    */
-   getMonthsLabelsFromLang(lang) {
-     this.MONTHS = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
-   }
-
-
-  /**
-   * Check if the given param is in timestamp format
-   * If not, return the current timestamp
-   * @param {*} timestamp 
-   */
-  checkTimestamp(timestamp){
-    if ((new Date(timestamp)).getTime() > 0){
-      return timestamp;
+    constructor(lang = "fr", options = {}) {
+        this.DAYS = [];
+        this.MONTHS = [];
+        this.DEFAULT_DATE_SEPARATOR = options.dateSeparator != null ? options.dateSeparator : "/";
+        this.DEFAULT_HOURS_MINUTES_SEPARATOR = options.hoursMinutesSeparator != null ? options.hourSeparator : ":";
+        this.DEFAULT_MINUTES_SECONDS_SEPARATOR = options.minutesSecondsSeparator != null ? options.hourSeparator : ":";
+        this.getDaysLabelsFromLang(lang);
+        this.getMonthsLabelsFromLang(lang);
     }
-    else{
-      console.error("Timestamp format not valid. Using current timestamp instead");
-      return this.getCurrentTimestamp();
+
+    /**
+     * Return the current timestamp
+     * Easy but useful when you're not used to JS date methods
+     */
+    getCurrentTimestamp() {
+        return new Date().getTime();
     }
-  }
 
-  /**
-   * Transform day number to a string wih '0' before if day's number < 10
-   * @param {*} date 
-   */
-  adjustDay(date){
-    var adjustedDay = Number(date);
-    if(date <= 9){
-      adjustedDay = "0" + date.toString();
+    /**
+     * get days labels from language
+     * To implement
+     * @param {*} lang 
+     */
+    getDaysLabelsFromLang(lang) {
+        this.DAYS = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
     }
-    return adjustedDay;
-  }
 
-  /**
-   * Transform month number to a string wih '0' before if month's number < 10
-   * @param {*} date 
-   */
-  adjustMonth(month){
-    var adjustedMonth = Number(month);
-    adjustedMonth ++;
-    if(adjustedMonth <= 9){
-      adjustedMonth = "0" + adjustedMonth.toString();
+    /**
+     * get days labels from language
+     * To implement
+     * @param {*} lang 
+     */
+    getMonthsLabelsFromLang(lang) {
+        this.MONTHS = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
     }
-    else{
-      adjustedMonth = adjustedMonth.toString();
+
+
+    /**
+     * Check if the given param is in timestamp format
+     * If not, return the current timestamp
+     * @param {*} timestamp 
+     */
+    checkTimestamp(timestamp) {
+        if ((new Date(timestamp)).getTime() > 0) {
+            return timestamp;
+        } else {
+            console.error("Timestamp format not valid. Using current timestamp instead");
+            return this.getCurrentTimestamp();
+        }
     }
-    return adjustedMonth;
-  }
 
-   /**
-    * 
-    * @param {*} timestamp timestamp to transform
-    * @param {*} options :
-    * separator : custom date separator (/ \ .), default : /
-    * fullyear : 2018 if true, else 18
-    * format : desired format. Can be 'DM' (day + separator + month) or 'MY' (month + separator + year). default (day + sep + month + sep + year)
-    * examples : 22/06/18, 22.06, ...
-    */
-   getFormattedDate(timestamp, options = {}){
-    timestamp = this.checkTimestamp(timestamp);
-   	var myDate = new Date(timestamp);
-   	var finaleSeparator = options.separator != null ? options.separator : this.DEFAULT_DATE_SEPARATOR;
-   	var year = myDate.getFullYear();
-   	if(options.fullyear === false){
-   		year = year.toString().substr(2,2);
-   	}
-   	var day = this.adjustDay(myDate.getDate());
-   	var month = this.adjustMonth(myDate.getMonth());
-
-   	var dateToReturn = "";
-
-   	if(options.format != null){
-   		switch(options.format){
-   			case "DM":
-   				dateToReturn = day + finaleSeparator + month;
-   				break;
-   			case "MY":
-   				dateToReturn = month + finaleSeparator + year;
-   				break;
-   			default:
-   				dateToReturn = day + finaleSeparator + month + finaleSeparator + year;
-   				break;
-   		}
-   	}
-   	else{
-   		dateToReturn = day + finaleSeparator + month + finaleSeparator + year;
-      }
-   	return dateToReturn;
-   }
-
-   /**
-    * To implement
-    * @param {*} timestamp 
-    * @param {*} options 
-    */
-   getHourMinutes(timestamp, options = {}){
-
-   }
-
-   
-  /**
-   * Return the given date to something more litteral
-   * example : Vendredi 22 juin 2018
-   * @param {*} timestamp timestamp to transform
-   * @param {*} options options :
-   * isDay : boolean, display the day (monday, tuesday, ...)
-   * isYear: boolean, display the month
-   * case : string, upper | lower | camel
-   * default format : 22 Juin
-   */
-   getVerboseDate(timestamp, options = {}){
-    timestamp = this.checkTimestamp(timestamp);
-    var month = this.getVerboseMonth(timestamp, options.case);
-    var dateFormatted = new Date(timestamp);
-    var day = this.adjustDay(dateFormatted.getDate());
-    var year = dateFormatted.getFullYear();
-        
-    var base = day + " " +  month;
-    if(options.isDay){
-      var verboseDay = this.getVerboseWeekDay(timestamp, options.case);
-      base = verboseDay + " " + base;
+    /**
+     * Transform day number to a string wih '0' before if day's number < 10
+     * @param {*} date 
+     */
+    adjustDay(date) {
+        var adjustedDay = Number(date);
+        if (date <= 9) {
+            adjustedDay = "0" + date.toString();
+        }
+        return adjustedDay;
     }
-    if(options.isYear){
-      base = base + " " + year;
+
+    /**
+     * Transform month number to a string wih '0' before if month's number < 10
+     * @param {*} date 
+     */
+    adjustMonth(month) {
+        var adjustedMonth = Number(month);
+        adjustedMonth++;
+        if (adjustedMonth <= 9) {
+            adjustedMonth = "0" + adjustedMonth.toString();
+        } else {
+            adjustedMonth = adjustedMonth.toString();
+        }
+        return adjustedMonth;
     }
-    return base;
-   }
+
+    /**
+     * 
+     * @param {*} timestamp timestamp to transform
+     * @param {*} options :
+     * separator : custom date separator (/ \ .), default : /
+     * fullyear : 2018 if true, else 18
+     * format : desired format. Can be 'DM' (day + separator + month) or 'MY' (month + separator + year). default (day + sep + month + sep + year)
+     * examples : 22/06/18, 22.06, ...
+     */
+    getFormattedDate(timestamp, options = {}) {
+        timestamp = this.checkTimestamp(timestamp);
+        var myDate = new Date(timestamp);
+        var finaleSeparator = options.separator != null ? options.separator : this.DEFAULT_DATE_SEPARATOR;
+        var year = myDate.getFullYear();
+        if (options.fullyear === false) {
+            year = year.toString().substr(2, 2);
+        }
+        var day = this.adjustDay(myDate.getDate());
+        var month = this.adjustMonth(myDate.getMonth());
+
+        var dateToReturn = "";
+
+        if (options.format != null) {
+            switch (options.format) {
+                case "DM":
+                    dateToReturn = day + finaleSeparator + month;
+                    break;
+                case "MY":
+                    dateToReturn = month + finaleSeparator + year;
+                    break;
+                default:
+                    dateToReturn = day + finaleSeparator + month + finaleSeparator + year;
+                    break;
+            }
+        } else {
+            dateToReturn = day + finaleSeparator + month + finaleSeparator + year;
+        }
+        return dateToReturn;
+    }
+
+    /**
+     * To implement
+     * @param {*} timestamp : timetamp to convert
+     * @param {*} options :
+     * separator : wanted separator between hour and minutes
+     */
+    getHoursMinutesSeconds(timestamp, options = {}) {
+        var finaleHoursMinutesSeparator = options.hourminSeparator != null ? options.hourminSeparator : this.DEFAULT_HOURS_MINUTES_SEPARATOR;
+        var returnedValue = this.getHours(timestamp) + finaleHoursMinutesSeparator + this.getMinutes(timestamp);
+        if(options.showSeconds === true){
+            var finaleMinutesSecondsSeparator = options.minsecSeparator != null ? options.minsecSeparator : this.DEFAULT_MINUTES_SECONDS_SEPARATOR;
+            returnedValue += finaleMinutesSecondsSeparator + this.getSeconds(timestamp);
+        }
+        return returnedValue;
+    }
+
+    /**
+     * Get hours from timestamp
+     * @param {*} timestamp 
+     */
+    getHours(timestamp) {
+        timestamp = this.checkTimestamp(timestamp);
+        var myDate = new Date(timestamp);
+        var hours = myDate.getHours();
+        return hours <= 9 ? "0" + hours : hours;
+    }
+
+    /**
+     * get minutes from timestamp
+     * @param {*} timestamp 
+     */
+    getMinutes(timestamp) {
+        timestamp = this.checkTimestamp(timestamp);
+        var myDate = new Date(timestamp);
+        var minutes = myDate.getMinutes();
+        return minutes <= 9 ? "0" + minutes : minutes;
+    }
+
+    /**
+     * get seconds from timestamp
+     * @param {*} timestamp 
+     */
+    getSeconds(timestamp){
+        timestamp = this.checkTimestamp(timestamp);
+        var myDate = new Date(timestamp);
+        var seconds = myDate.getSeconds();
+        return seconds <= 9 ? "0" + seconds : seconds;
+    }
 
 
-   /**
-    * Get the name of a day
-    * @param {*} timestamp timestamp to convert
-    * @param {*} format camel (default) | upper | lower
-    */
-   getVerboseWeekDay(timestamp, format = "camel"){
-    timestamp = this.checkTimestamp(timestamp);
-    var myDate = new Date(timestamp);
-    var day = myDate.getDay();
-    var stringDate = this.DAYS[day];
-    if(format == "upper"){
-      stringDate = stringDate.toUpperCase();
+    /**
+     * Return the given date to something more litteral
+     * example : Vendredi 22 juin 2018
+     * @param {*} timestamp timestamp to transform
+     * @param {*} options options :
+     * isDay : boolean, display the day (monday, tuesday, ...)
+     * isYear: boolean, display the month
+     * case : string, upper | lower | camel
+     * default format : 22 Juin
+     */
+    getVerboseDate(timestamp, options = {}) {
+        timestamp = this.checkTimestamp(timestamp);
+        var month = this.getVerboseMonth(timestamp, options.case);
+        var dateFormatted = new Date(timestamp);
+        var day = this.adjustDay(dateFormatted.getDate());
+        var year = dateFormatted.getFullYear();
+
+        var base = day + " " + month;
+        if (options.isDay) {
+            var verboseDay = this.getVerboseWeekDay(timestamp, options.case);
+            base = verboseDay + " " + base;
+        }
+        if (options.isYear) {
+            base = base + " " + year;
+        }
+        return base;
     }
-    else if(format == "lower"){
-      stringDate = stringDate.toLowerCase();
+
+
+    /**
+     * Get the name of a day
+     * @param {*} timestamp timestamp to convert
+     * @param {*} format camel (default) | upper | lower
+     */
+    getVerboseWeekDay(timestamp, format = "camel") {
+        timestamp = this.checkTimestamp(timestamp);
+        var myDate = new Date(timestamp);
+        var day = myDate.getDay();
+        var stringDate = this.DAYS[day];
+        if (format == "upper") {
+            stringDate = stringDate.toUpperCase();
+        } else if (format == "lower") {
+            stringDate = stringDate.toLowerCase();
+        }
+        return stringDate;
     }
-    return stringDate;
-   }
 
     /**
      * Get the name of a month
      * @param {*} timestamp timestamp to convert
      * @param {*} format camel (default) | upper | lower
      */
-   getVerboseMonth(timestamp, format = "camel"){
-    timestamp = this.checkTimestamp(timestamp);
-    var myDate = new Date(timestamp);
-    var month = myDate.getMonth();
-    var stringMonth = this.MONTHS[month];
-    if (format == "upper") {
-      stringMonth = stringMonth.toUpperCase();
-    } else if (format == "lower") {
-      stringMonth = stringMonth.toLowerCase();
+    getVerboseMonth(timestamp, format = "camel") {
+        timestamp = this.checkTimestamp(timestamp);
+        var myDate = new Date(timestamp);
+        var month = myDate.getMonth();
+        var stringMonth = this.MONTHS[month];
+        if (format == "upper") {
+            stringMonth = stringMonth.toUpperCase();
+        } else if (format == "lower") {
+            stringMonth = stringMonth.toLowerCase();
+        }
+        return stringMonth;
     }
-    return stringMonth;
-   }
 }
